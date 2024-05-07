f92ff519ea6100b4a387437ee7ac7c0bf9ee3c64
Nagamani C Gunjal <30571369+nagamanicg@users.noreply.github.com>
2024-05-07 05:44:10 +0000

    Modified the contents

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,10 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
+    email: 'nagamanicg229@gmail.com',
+    phone: '+918277100514',
+    address: '8000, Greeenbelt Station Pkwy, Greenbelt, MD',
+    github: 'https://github.com/nagamanicg',
+    linkedIn: 'https://www.linkedin.com/in/nagamanicgunjal/',
+    twitter: 'https://twitter.com/nagamanicg',
     stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
     devUsername: "said7388"
 }
