ae8cc6e0fd01629cf47956798af6b5f83d4bbd42
Nagamani C Gunjal <30571369+nagamanicg@users.noreply.github.com>
2024-05-07 06:47:53 +0000

    changes on content

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -14,16 +14,6 @@ function Footer() {
           <p className="text-sm">
             © Developer Portfolio by <Link target="_blank" href="http://www.linkedin.com/in/nagamanicgunjal/" className="text-[#16f2b3]">Nagamani</Link>
           </p>
-          <div className="flex items-center gap-5">
-            <Link
-              target="_blank"
-              href="https://github.com/said7388/developer-portfolio"
-              className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
-            >
-              <IoStar />
-              <span>Star</span>
-            </Link>
-          </div>
         </div>
       </div>
     </div >
