49752eec47b45f8ec7ea4189147ebe3bb59dedfe
nagamani <nagamanicg229@gmail.com>
2025-07-16 22:12:08 -0400

    delete files

--- utils/data/contactsData.js
+++
@@ -1,9 +0,0 @@
-export const contactsData = {
-    email: 'nagamanicg229@gmail.com',
-    phone: '+918277100514',
-    address: '8000, Greeenbelt Station Pkwy, Greenbelt, MD',
-    github: 'https://github.com/nagamanicg',
-    linkedIn: 'https://www.linkedin.com/in/nagamanicgunjal/',
-    twitter: 'https://twitter.com/nagamanicg',
-    devUsername: "nagamanicg"
-}
